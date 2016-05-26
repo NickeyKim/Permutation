be468ad59375e4506e50ed77936cc6253817183a
NickeyKim <mmf760@gmail.com>
2016-05-26 15:00:59 +0900

    20160526
    20160526

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ namespace Permutation
 {
 	public class Permutations
 	{
-		/*if passed the last position,
-		 * 	print String
+		/*if it passed the last position,
+		 * 	print the String
 		 * 	return
 		 *for each character in the string
 		 *	if it was used, move on next char
 		 *	input the char to current position
 		 *		mark the char used
-		 *		CurPos+1, recursion to the rest,
+		 *		CurPos+1, 'recursion' to the rest,
 		 *      mark the char unused
 		 */
 		private Boolean[] used;   //in order to mark if used or unused
@@ -22,7 +22,7 @@ namespace Permutation
 
 		public Permutations (string str){
 			StrIn = str;
-			used = new Boolean[ StrIn.Length];
+			used = new Boolean[ StrIn.Length];           //make bools as the number of the length
 		}
 		public void Permute()
 		{
@@ -35,20 +35,18 @@ namespace Permutation
 			{
 				if (used [i]) continue;
 				StrOut.Append (StrIn [i]);
-				used [i] = true;                             //marking it used-true
+				used [i] = true;                             //marking it used->true
 				Permute ();
 				used [i] = false;
 				StrOut.Remove (StrOut.Length-1, 1);
 			}
 		}
-
-
 		public static void Main (string[] args)
 		{
 			Permutations Perm = new Permutations ("abcde");
 			Perm.Permute ();
 
-			Console.WriteLine ("Hello World!");
+			//Console.WriteLine ("Hello World!");
 		}
 	}
 }
